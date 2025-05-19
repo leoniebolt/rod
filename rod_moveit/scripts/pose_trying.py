028f3ad87446a840d43376fa5b0b344763782863
leoniebolt <re24m008@technikum-wien>
2025-05-19 16:17:11 +0200

    autonomous robots

--- rod_moveit/scripts/pose_trying.py
+++ rod_moveit/scripts/pose_trying.py
@@ -6,10 +6,23 @@ import rospy
 import moveit_commander
 import geometry_msgs.msg
 import numpy as np
-from gazebo_ros_link_attacher.srv import Attach, AttachRequest, AttachResponse
 
 class DemoRobot:
-    def __init__(self, groupname):
+    
+    
+    def __init__(self, nodename="DemoRobot", groupname="robotGroup"):
+        rospy.init_node(nodename, anonymous=False)
+        moveit_commander.roscpp_initialize(sys.argv)
+        self.robot = moveit_commander.RobotCommander()
+        self.move_group = moveit_commander.MoveGroupCommander(groupname)
+        
+        #Acceleration and Speed factors
+        self.move_group.set_max_acceleration_scaling_factor(1)
+        self.move_group.set_max_velocity_scaling_factor(1)
+        
+        self.move_group.set_goal_tolerance(0.5)
+        self.goals = []
+        
         self.move_group = moveit_commander.MoveGroupCommander(groupname)
         self.groupname = groupname
 
@@ -17,253 +30,216 @@ class DemoRobot:
         self.move_group.set_max_velocity_scaling_factor(1.0)
         self.move_group.set_goal_tolerance(0.001)
 
-        self.pose_goals = []
-        self.joint_goals = []
 
     def get_current_pose(self):
         pose = self.move_group.get_current_pose().pose
-        print(f"[{self.groupname}] Current Pose:")
+        print(f"[{self.groupname}] Aktuelle Pose:")
         print(f"  Position: x={pose.position.x:.3f}, y={pose.position.y:.3f}, z={pose.position.z:.3f}")
-        print(f"  Orientation: x={pose.orientation.x:.3f}, y={pose.orientation.y:.3f}, "
+        print(f"  Orientierung: x={pose.orientation.x:.3f}, y={pose.orientation.y:.3f}, "
               f"z={pose.orientation.z:.3f}, w={pose.orientation.w:.3f}")
         return pose
 
-    def set_pose_target(self, x, y, z, roll, pitch, yaw):
-        pose_stamped = geometry_msgs.msg.PoseStamped()
 
-        # RPY → Quaternion
-        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    def move_to_pose(self, pose, label=""):
+        print(f"[{self.groupname}] Bewege zu Pose: {label}")
+        self.move_group.set_pose_target(pose)
+        self.move_group.go(wait=True)
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
 
-        # Setze Pose
-        pose_stamped.pose.position.x = x
-        pose_stamped.pose.position.y = y
-        pose_stamped.pose.position.z = z
-        pose_stamped.pose.orientation.x = qx
-        pose_stamped.pose.orientation.y = qy
-        pose_stamped.pose.orientation.z = qz
-        pose_stamped.pose.orientation.w = qw
-
-        # Setze das Referenzkoordinatensystem je nach Roboter
-        if self.groupname == "scara":
-            pose_stamped.header.frame_id = "scara_basejoint"
-        elif self.groupname == "sixaxis":
-            pose_stamped.header.frame_id = "sixaxis_basejoint"
-        else:
-            pose_stamped.header.frame_id = "base_link"
-
-        # Zielpose speichern
-        self.pose_goals.append(copy.deepcopy(pose_stamped))
-
-
-
-    def set_joint_target(self, joint_values):
-        self.joint_goals.append(joint_values)
 
     def move(self):
-        # Joint goals
-        for joints in self.joint_goals:
-            print(f"[{self.groupname}] Moving to joint target: {joints}")
-            self.move_group.go(joints, wait=True)
-            self.move_group.stop()
 
-        # Pose goals
-        for pose in self.pose_goals:
-            print(f"[{self.groupname}] Moving to pose target...")
-
-            # Setze Planungskonfiguration
-            self.move_group.set_planning_time(2.0)  # z. B. 10 Sekunden Planungszeit
-            self.move_group.set_num_planning_attempts(10)  # z. B. 10 Versuche
-
-            self.move_group.set_pose_target(pose)
-            success = self.move_group.go(wait=True)
-
-            if not success:
-                rospy.logwarn(f"[{self.groupname}] Bewegung zum Pose-Ziel fehlgeschlagen!")
-
+        self.move_group.clear_pose_targets()
+        try:
+            for g in self.goals:
+                self.move_group.set_pose_target(g)
+                self.move_group.go(wait=True)
+        except:
+            print("Targets not reachable")
+        finally:
+            print("Stopping")
             self.move_group.stop()
             self.move_group.clear_pose_targets()
+            self.goals = []
 
-        # Reset
-        self.joint_goals = []
-        self.pose_goals = []
+
+    def set_pose_reference_frame(self, reference_frame):
+        """Set the reference frame to assume for poses of end-effectors"""
+        self._g_g.set_pose_reference_frame(reference_frame)
+
+# Funktion anpassen! Und evtl. nicht verwenden -> konvertiert Koordinaten von einem reference frame in ein anderes frame (Welt in Roboter zB)
+    def printPose(self,reference_frame=None):
+        self.cp = self.move_group.get_current_pose()
+        #print(self.move_group.get_pose_reference_frame())
+        if reference_frame is None:
+            print("Current pose for TCP <",self.groupname,"> in Reference frame <",self.cp.header.frame_id,"> :")
+            print("x: ",self.cp.pose.position.x)
+            print("y: ",self.cp.pose.position.y)
+            print("z: ",self.cp.pose.position.z)
+            print("qx: ",self.cp.pose.orientation.x)
+            print("qy: ",self.cp.pose.orientation.y)
+            print("qz: ",self.cp.pose.orientation.z)
+            print("qw: ",self.cp.pose.orientation.w)
+        else:
+            #try:  
+            self.tf_buffer.can_transform(reference_frame, self.cp.header.frame_id, rospy.Time(0), rospy.Duration(10.0))
+            transformed_pose = self.tf_buffer.transform(self.cp, reference_frame)
+            print("Current pose for TCP <",self.groupname,"> in Reference frame <",reference_frame,"> :")
+            print("x: ",transformed_pose.pose.position.x)
+            print("y: ",transformed_pose.pose.position.y)
+            print("z: ",transformed_pose.pose.position.z)
+            print("qx: ",transformed_pose.pose.orientation.x)
+            print("qy: ",transformed_pose.pose.orientation.y)
+            print("qz: ",transformed_pose.pose.orientation.z)
+            print("qw: ",transformed_pose.pose.orientation.w)
+            #except:
+            #    print("Reference Frame does not exist!")
 
 
 if __name__ == "__main__":
-    rospy.init_node("multi_robot_demo", anonymous=False)
-    moveit_commander.roscpp_initialize(sys.argv)
-
-## Link Attacher (aus attach.py aus https://github.com/pal-robotics/gazebo_ros_link_attacher)
-#    rospy.loginfo("Creating ServiceProxy to /link_attacher_node/attach")
-#    attach_srv = rospy.ServiceProxy('/link_attacher_node/attach',
-#                                    Attach)
-#    attach_srv.wait_for_service()
-#    rospy.loginfo("Created ServiceProxy to /link_attacher_node/attach")
     
-## Link Detacher (aus detach.py aus https://github.com/pal-robotics/gazebo_ros_link_attacher)
-#    rospy.loginfo("Creating ServiceProxy to /link_attacher_node/detach")
-#    detach_srv = rospy.ServiceProxy('/link_attacher_node/detach',
-#                                    Attach)
-#    detach_srv.wait_for_service()
-#    rospy.loginfo("Created ServiceProxy to /link_attacher_node/detach")
-    
-
-    # Roboter-Instanzen um die verschiedenen movegroups anzusprechen
-    scara = DemoRobot(groupname="scara")
+    # Roboter-Instanzen
     sixaxis = DemoRobot(groupname="sixaxis")
+    scara = DemoRobot(groupname="scara")
+
+
+    # Zielposen für Six-Axis
+    sa_poses = {
+        # WERTE NICHT VERÄNDERN
+        "sixaxis_home": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=-0.864, y=-1.120, z=1.587),
+            orientation=geometry_msgs.msg.Quaternion(x=-0.702, y=0.000, z=-0.712, w=0.000)
+        ),
+
+
+
+        "sa_above_pick_up": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=-0.869, y=-1.125, z=1.403),
+            orientation=geometry_msgs.msg.Quaternion(x=0.686, y=-0.727, z=-0.006, w=0.003)
+        ),
+        "sa_pick_up": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=-0.875, y=-1.115, z=1.196),
+            orientation=geometry_msgs.msg.Quaternion(x=0.691, y=-0.723, z=-0.008, w=0.002)
+        ),
+        "sa_above_place": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=-2.425, y=-1.040, z=1.067),
+            orientation=geometry_msgs.msg.Quaternion(x=-0.715, y=-0.699, z=0.004, w=0.012)
+        ),
+        "sa_place": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=-2.771, y=-1.029, z=0.421),
+            orientation=geometry_msgs.msg.Quaternion(x=-0.717, y=-0.697, z=0.007, w=0.018)
+        )
+    }
+
+    # Zielposen für SCARA
+    s_poses = {
+        # WERTE NICHT VERÄNDERN!!!!
+        "scara_home": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=2.959, y=-0.638, z=1.2),
+            orientation=geometry_msgs.msg.Quaternion(x=0.998, y=-0.065, z=0.000, w=0.000)
+        ),
+
+        # weiteste was ich bis jetzt gekommen bin :
+        # position=geometry_msgs.msg.Point(x=2.55, y=-0.2, z=1.2),
+        # orientation=geometry_msgs.msg.Quaternion(x=0.998, y=-0.065, z=0.000, w=0.000)
+
+        "s_above_pick_up": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=2.55, y=-0.2, z=1.2),
+            orientation=geometry_msgs.msg.Quaternion(x=0.998, y=-0.065, z=0.000, w=0.000)
+        ),
+        "s_pick_up": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=2.55, y=-0.2, z=1.15),
+            orientation=geometry_msgs.msg.Quaternion(x=0.998, y=-0.065, z=0.000, w=0.000)
+        ),
+        "s_above_place": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=2.469, y=-1.064, z=1.243),
+            orientation=geometry_msgs.msg.Quaternion(x=0.998, y=-0.065, z=0.000, w=0.000)
+        ),
+        "s_place": geometry_msgs.msg.Pose(
+            position=geometry_msgs.msg.Point(x=2.469, y=-1.064, z=1.15),
+            orientation=geometry_msgs.msg.Quaternion(x=0.998, y=-0.065, z=0.000, w=0.000)
+        )
+    }
+
+
+    # SCARA
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["scara_home"], "SCARA home")
+    #scara.get_current_pose()
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["s_above_pick_up"], "SCARA above_pick_up")
+    #scara.get_current_pose()
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["s_pick_up"], "SCARA pick_up")
+    #scara.get_current_pose()
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["s_above_pick_up"], "SCARA above_pick_up")
+    #scara.get_current_pose()
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["s_above_place"], "SCARA above_place")
+    #scara.get_current_pose()
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["s_place"], "SCARA place")
+    #scara.get_current_pose()
+
+    #print("\n. \n. \n")
+    #scara.move_to_pose(s_poses["s_above_place"], "SCARA above_place")
+    #scara.get_current_pose()
+
     
-        #ATTACH Smartphone_Box an Drehtisch (code aus https://github.com/pal-robotics/gazebo_ros_link_attacher)
-#    rospy.loginfo("Attaching Smartphone_Box and drehtisch")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link1_d"
-#    attach_srv.call(req)
-        
-    # Joint-Ziel für scara
-    sixaxis.set_joint_target([0.7, 0.0, 0.0, 0.0])
-    #Bewegung ausführen
-    sixaxis.move()
 
-    # Joint-Ziel für rothbot
-    scara.set_joint_target([1.04, -1.76, 0.05, -0.05, 0.0, -0.35])
-    scara.set_joint_target([1.04, -1.76, 0.05, -0.05, 0.145, -0.35])
-    scara.move()
+    # SIXAXIS
+    print("\n. \n. \n")
+    sixaxis.get_current_pose()
     
-        # ATTACH Smartphone an rothbot
-#    rospy.loginfo("Attaching Smartphone and rothbot")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link6_r"
-#    attach_srv.call(req)
-#    rospy.sleep(0.5)
 
-    scara.set_joint_target([1.04, -1.76, 0.05, -0.05, 0.0, -0.35])
-    scara.set_joint_target([-1.05, -0.80, -0.01, 0.01, 0.0, -0.282])
-    # Hier einmal ein pose_target wollte anfangs pose_targets machen habe mich umentschienden joint targets zu verwenden weil es einfacher ist richitge positionen zu setzen (in diesem fall)
-    # Implementierung ist trozdem vorhanden diese ezile macht genau das gleiche wie das joint target darunter
-    #rothbot.set_pose_target(0.00, 0.26, 1.47, 3.14, 0.0, 1.57) #Habe pose_targets im code nur ienmal zum funktionieren gebracht, Im hmi konnte ich die position erreichen
-    scara.set_joint_target([-1.05, -0.80, -0.01, 0.01, 0.02, -0.282])
-    scara.move()
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sixaxis_home"], "SIXAXIS home")
+    sixaxis.get_current_pose()
+
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sa_above_pick_up"], "SIXAXIS above_pick_up")
+    sixaxis.get_current_pose()
     
-        # DETACH Smartphone von rothbot
-#    rospy.loginfo("Detach Smartphone and rothbot")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link6_r"
-#    detach_srv.call(req)
-#    rospy.sleep(0.5)
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sa_pick_up"], "SIXAXIS pick_up")
+    sixaxis.get_current_pose()
+
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sa_above_pick_up"], "SIXAXIS above_pick_up")
+    sixaxis.get_current_pose()
+
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sa_above_place"], "SIXAXIS above_place")
+    sixaxis.get_current_pose()
     
-        # ATTACH Smartphone an Smartphone_box
-#    rospy.loginfo("Attaching Smartphone and Smartphone_box")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "Smartphone_Box"
-#    req.link_name_2 = "link"
-#    attach_srv.call(req)
-#    rospy.sleep(0.5)
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sa_place"], "SIXAXIS place")
+    sixaxis.get_current_pose()
 
-    scara.set_joint_target([-1.05, -0.80, -0.01, 0.01, 0.0, -0.282])
-    scara.set_joint_target([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-    # Bewegung ausführen
-    scara.move()
-
-    # Joint-Ziel für drehtisch
-#    drehtisch.set_joint_target([3.14])  # 180°
-#    drehtisch.move()
-
-    # Joint-Ziel für sixaxis
-    sixaxis.set_joint_target([0.0, 0.07, 0.0, -0.1])
-    sixaxis.move()
-    
-        # DETACH Smartphone_Box von drehtisch
-#    rospy.loginfo("Detach Smartphone_Box and rothbot")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link1_d"
-#    detach_srv.call(req)
-#    rospy.sleep(0.5)
-    
-        # ATTACH Smartphone_Box an sixaxis
-#    rospy.loginfo("Attaching Smartphone_Box and hunfagltron")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link4_h"
-#    attach_srv.call(req)
-#    rospy.sleep(0.5)
-
-    sixaxis.set_joint_target([1.57, 0.0, 0.0, 0.0])
-    sixaxis.set_joint_target([1.57, -0.47, -0.53, 0.47])
-    sixaxis.move()
-    
-        # DETACH von sixaxis
-#    rospy.loginfo("Attaching Smartphone_Box and hunfagltron")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link4_h"
-#    detach_srv.call(req)
-#    rospy.sleep(0.5)
-
-    sixaxis.set_joint_target([1.57, 0.0, 0.0, 0.0])
-    sixaxis.set_joint_target([0.0, 0.07, 0.0, -0.1])
-    sixaxis.set_joint_target([-1.57, 0.0, 0.0, 0.0])
-    sixaxis.set_joint_target([-1.57, -0.47, -0.53, 0.47])
-    sixaxis.move()
-    
-        # ATTACH an sixaxis
-#    rospy.loginfo("Attaching Smartphone_Box and hunfagltron")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box_neu"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link4_h"
-#    attach_srv.call(req)
-#    rospy.sleep(0.5)
-
-    sixaxis.set_joint_target([-1.57, 0.0, 0.0, 0.0])
-    sixaxis.set_joint_target([0.0, 0.07, 0.0, -0.1])
-    sixaxis.move()
-    
-        # DETACH von sixaxis
-#    rospy.loginfo("Attaching Smartphone_Box and hunfagltron")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box_neu"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link4_h"
-#    detach_srv.call(req)
-#    rospy.sleep(0.5)
-
-    sixaxis.set_joint_target([0.7, 0.0, 0.0, 0.0])
-    sixaxis.move()
-
-        # ATTACH an drehtisch
-#    rospy.loginfo("Attaching Smartphone_Box_neu and drehtisch")
-#    req = AttachRequest()
-#    req.model_name_1 = "Smartphone_Box_neu"
-#    req.link_name_1 = "link"
-#    req.model_name_2 = "robot"
-#    req.link_name_2 = "link1_d"
-#    attach_srv.call(req)
-#    rospy.sleep(0.5)
+    print("\n. \n. \n")
+    sixaxis.move_to_pose(sa_poses["sa_above_place"], "SIXAXIS above_place")
+    sixaxis.get_current_pose()
 
 
+    #print("\n. \n. \n")
+    #sixaxis.move_to_pose(sa_poses["sa_above_pick_up"], "SIXAXIS above_pick_up")
+    #sixaxis.get_current_pose()
 
-    # Joint-Ziel für drehtisch
-#    drehtisch.set_joint_target([0.0])  # 0°
-#    drehtisch.move()
-    
+    #print("\n. \n. \n")
+    #sixaxis.move_to_pose(sa_poses["sa_pick_up"], "SIXAXIS pick_up")
+    #sixaxis.get_current_pose()
+
+    #sixaxis.move_to_pose(sa_poses["pre_post_pick_up"], "SixAxis pre_post_pick_up")
+    #sixaxis.move_to_pose(sa_poses["pick_up"], "SixAxis pick_up")
+    #sixaxis.move_to_pose(sa_poses["pre_post_pick_up"], "SixAxis pre_post_pick_up")
+    #sixaxis.move_to_pose(sa_poses["pre_post_place"], "SixAxis pre_post_place")
+    #sixaxis.move_to_pose(sa_poses["place"], "SixAxis place")
+    #sixaxis.move_to_pose(sa_poses["pre_post_place"], "SixAxis pre_post_place")
